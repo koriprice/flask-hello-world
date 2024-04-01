04397276ac756f1318a3a5fa9e7ed347843e9b30
Kori Price <kopr2422@colorado.edu>
2024-04-01 02:29:40 +0000

    db test added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+import psycopg2
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Kori Price in 3308'
+
+@app.route("/db_test")
+def db_test():
+    conn = psycopg2.connect("postgres://lab10_db_dj5p_user:Gzt7PjRQJ45ne27wUD6P52m6bJiTJ5bE@dpg-co51ku779t8c739a5ngg-a/lab10_db_dj5p")
+    conn.close()
+    return "Database Connection Successful"

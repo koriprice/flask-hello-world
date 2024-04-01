3a81bcd262d4dc3a31a73fb45d12c334bacf8915
Kori Price <kopr2422@colorado.edu>
2024-04-01 02:48:44 +0000

    create db added

--- app.py
+++ app.py
@@ -12,3 +12,22 @@ def db_test():
     conn = psycopg2.connect("postgres://lab10_db_dj5p_user:Gzt7PjRQJ45ne27wUD6P52m6bJiTJ5bE@dpg-co51ku779t8c739a5ngg-a/lab10_db_dj5p")
     conn.close()
     return "Database Connection Successful"
+
+@app.route("/db_create")
+def db_create():
+    conn = psycopg2.connect("postgres://lab10_db_dj5p_user:Gzt7PjRQJ45ne27wUD6P52m6bJiTJ5bE@dpg-co51ku779t8c739a5ngg-a/lab10_db_dj5p")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball (
+    First varchar(255),
+    Last varchar(255),
+    City varchar(255),
+    Name varchar(255),
+    Number int
+    );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+    
+    
